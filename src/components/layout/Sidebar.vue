feec639d7bb7ab18f870d6d2f21588f50b420aff
myungyun <myungyun.cho82@gmail.com>
2020-07-05 23:00:18 +0900

    fix side-bar sidebar.vue

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <div class='left'>
-      <b-nav vertical class="w-25">
-        <b-nav-item @mouseover="mouseOver" active :to="{ path: '/'}">Home</b-nav-item>
-        <b-nav-item active :to="{ path: '/inventory'}">Inventory</b-nav-item>
-        <b-nav-item :to="{ path: '/host'}">Host</b-nav-item>
-        <b-nav-item active :to="{ path: '/template'}">Playbook Template</b-nav-item>
-        <b-nav-item :to="{ path: '/ahtemplate'}">ADHOC Template</b-nav-item>
-        <b-nav-item :to="{ path: '/credential'}">Credential</b-nav-item>
-        <b-nav-item active :to="{ path: '/job'}">Job</b-nav-item>
+      <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item @mouseover="mouseOver" active :to="{ path: '/'}">Home</b-nav-item>
+              <b-nav-item active :to="{ path: '/inventory'}">Inventory</b-nav-item>
+              <b-nav-item :to="{ path: '/host'}">Host</b-nav-item>
+              <b-nav-item active :to="{ path: '/template'}">Playbook Template</b-nav-item>
+              <b-nav-item :to="{ path: '/ahtemplate'}">ADHOC Template</b-nav-item>
+              <b-nav-item :to="{ path: '/credential'}">Credential</b-nav-item>
+              <b-nav-item active :to="{ path: '/job'}">Job</b-nav-item>
+        </b-nav>
+      </nav>
         <!-- <b-nav-item disabled>Disabled</b-nav-item> -->
-      </b-nav>
     </div>
   </div>
 </template>
